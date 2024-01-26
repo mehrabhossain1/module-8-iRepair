91a35da78f7ed05581703d6ab778caac926b81d8
mehrabhossain1 <mehrab.munna00@gmail.com>
2024-01-26 09:12:27 +0600

    8.5

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,5 +1,17 @@
-const Container = ({ children }) => {
-  return <div className="w-full max-w-[1220px] px-5 mx-auto">{children}</div>;
+import { cn } from '@/lib/utils';
+import { ReactNode } from 'react';
+
+type TContainerProps = {
+  children: ReactNode;
+  className?: string;
+};
+
+const Container = ({ children, className }: TContainerProps) => {
+  return (
+    <div className={cn('w-full max-w-[1230px] px-5 mx-auto', className)}>
+      {children}
+    </div>
+  );
 };
 
 export default Container;
