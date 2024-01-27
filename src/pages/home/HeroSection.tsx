3a6000aea4c471285fde2c24f2096e26721e135a
mehrabhossain1 <mehrab.munna00@gmail.com>
2024-01-27 12:25:32 +0600

    8.7

--- src/pages/home/HeroSection.tsx
+++ src/pages/home/HeroSection.tsx
@@ -1,24 +1,48 @@
 import Container from '@/components/Container';
 import { Button } from '@/components/ui/button';
 import macbook from '@/assets/images/macbook-exposed.png';
+import { motion } from 'framer-motion';
 
 // ctrl + x = del entire line
+
+const intro = {
+  hidden: { opacity: 0 },
+  visible: {
+    opacity: 1,
+    transition: {
+      duration: 2,
+      staggerChildren: 1,
+    },
+  },
+};
+const introChildren = {
+  hidden: { opacity: 0, y: -500 },
+  visible: { opacity: 1, y: 0 },
+};
 
 const HeroSection = () => {
   return (
     <Container className="h-[calc(100vh-64px)] grid grid-cols-1 lg:grid-cols-2 place-content-center">
-      <div>
-        <h1 className="text-5xl lg:text-8xl font-bold text-nowrap">
+      <motion.div variants={intro} initial="hidden" animate="visible">
+        <motion.h1
+          className="text-5xl lg:text-8xl font-bold text-nowrap"
+          variants={introChildren}
+        >
           <span className="text-gray ">Don't worry,</span>
           <br />
           <span>We'll fix it.</span>
-        </h1>
-        <p className="max-w-[50ch] mt-10 mb-5 text-lg">
+        </motion.h1>
+        <motion.p
+          className="max-w-[50ch] mt-10 mb-5 text-lg"
+          variants={introChildren}
+        >
           Welcome to <span>iRepair</span>, your one-stop place for all kinds of
           <span>Macbook repairs</span> and diagnostics.
-        </p>
-        <Button>Book a service</Button>
-      </div>
+        </motion.p>
+        <motion.div variants={introChildren}>
+          <Button>Book a service</Button>
+        </motion.div>
+      </motion.div>
 
       <div className="mt-10 w-3/4 lg:w-full mx-auto">
         <img
