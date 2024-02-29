5d862ec0a4cf6be5a6b1da511967ba343d1286ef
mehrabhossain1 <mehrab.munna00@gmail.com>
2024-02-29 20:03:32 +0600

    get data from backend in Service List compo

--- src/pages/admin/ServiceList.tsx
+++ src/pages/admin/ServiceList.tsx
@@ -1,4 +1,15 @@
+import { useEffect, useState } from 'react';
+
 const ServiceList = () => {
+  const [data, setData] = useState([]);
+  console.log(data);
+
+  useEffect(() => {
+    fetch('http://localhost:5000/api/v1/services')
+      .then((res) => res.json())
+      .then((data) => setData(data.data));
+  }, []);
+
   return (
     <div>
       <h1>This is ServiceList component</h1>
