bb0b887c3db7806adcdf4f3ef976f46cc933df89
mehrabhossain1 <mehrab.munna00@gmail.com>
2024-03-01 23:20:43 +0600

    post service

--- src/pages/admin/AddService.tsx
+++ src/pages/admin/AddService.tsx
@@ -1,7 +1,46 @@
+import { Button } from '@/components/ui/button';
+import { useMutation } from '@tanstack/react-query';
+import { FormEvent, useState } from 'react';
+
 const AddService = () => {
+  const [serviceName, setServiceName] = useState('');
+
+  const { mutateAsync, isError, isSuccess } = useMutation({
+    mutationFn: async (data) => {
+      return await fetch('http://localhost:5000/api/v1/services', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      });
+    },
+  });
+
+  console.log({ isError, isSuccess });
+
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+
+    const serviceData = {
+      name: serviceName,
+      description: 'Replace broken screens',
+      devices: ['Smartphone', 'Laptop', 'Tablet'],
+      price: 100.0,
+    };
+    console.log(serviceData);
+
+    await mutateAsync(serviceData);
+
+    console.log('done');
+  };
+
   return (
     <div>
-      <h1>This is AddService component</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" onChange={(e) => setServiceName(e.target.value)} />
+        <Button type="submit">Submit</Button>
+      </form>
     </div>
   );
 };
